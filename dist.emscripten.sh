e4647d3838aef6d139e9a6be60c67268258b3ed4
Valeriano A.R <valen.alfonso@gmail.com>
2016-04-16 14:28:00 +0200

    Fixes to the build system

--- dist.emscripten.sh
+++ dist.emscripten.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-make -f Makefile.emscripten target=release clean
-make -f Makefile.emscripten target=release
+#make -f Makefile.emscripten target=release clean
+#make -f Makefile.emscripten target=release
+
+make -f Makefile.emscripten clean
+make -f Makefile.emscripten 
 
 mkdir -p DIST/web
 cp -Rv web/* DIST/web/
-cp -v build-emscripten-release/game.* DIST/web/
+#cp -v build-emscripten-release/game.* DIST/web/
+cp -v build-emscripten/game.* DIST/web/
